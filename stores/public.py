4c1c60cdb4a045000e14f1749d6a83cccce5ab09
stefanosgregory <gregorystefanos@gmail.com>
2021-07-10 00:07:23 +0300

    ~v3.3 add 2 more ps5 editions at public store.

--- stores/public.py
+++ stores/public.py
@@ -11,28 +11,28 @@ def check_inventory():
     print("~Public:")
 
     ps5_link = "https://www.public-cyprus.com.cy/cy/common/updateProductInfo.jsp?product="
-    ps5 = ["prod10238545pp", "prod10810253pp"]
-    flag = "Standard edition: "
+    ps5 = ["prod10238545pp", "prod10810253pp", "prod13802366pp", "prod13923508pp"]
+    ps5_names = ["Standard edition: ", "Digital  edition: ", "Ratchet  edition: ", "2 Games  edition: "]
     
-    for item in ps5:
+    for item in range(len(ps5)):
         try:
-            response = requests.get(ps5_link + item).json()
+            response = requests.get(ps5_link + ps5[item]).json()
             stock = response['productInfo'][0]['stockRule']
 
             if stock == "εξαντλήθηκε!":
-                print(flag + bcolors.FAIL + "Out of stock" + bcolors.RESET)
+                print(ps5_names[item] + bcolors.FAIL + "Out of stock" + bcolors.RESET)
             else:
-                print(flag + bcolors.OK + "In stock at Public" + bcolors.RESET)
+                print(ps5_names[item] + bcolors.OK + "In stock at Public" + bcolors.RESET)
 
-                notify("PS5 " + flag + " in stock at Public")
+                notify("PS5 " + ps5_names[item] + " in stock at Public")
 
-            flag = "Digital  edition: "
         except:
-            print(flag + bcolors.WARNING + "Error searching for item" + bcolors.RESET)
+            print(ps5_names[item] + bcolors.WARNING + "Error searching for item" + bcolors.RESET)
             
-            notify("Error PS5 " + flag + " public")
+            notify("Error PS5 " + ps5_names[item] + " public")
 
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
 
 
 
